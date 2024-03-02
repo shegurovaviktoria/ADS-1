7c175990eda688b51ff2f992cdcb00ab151e5802
viktoria shegurova <137227112+shegurovaviktoria@users.noreply.github.com>
2024-03-02 20:07:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,16 +5,59 @@
 
 bool checkPrime(uint64_t value) {
 // вставьте код функции
+  uint64_t i;
+	if (value < 2) return false;
+	else if (value == 2) return true;
+	else {
+		for (i = 2; i < value; i++) {
+			if (value % i == 0) return false;
+		}
+	}
+	return true;
 }
 
 uint64_t nPrime(uint64_t n) {
 // вставьте код функции
+  uint64_t mas[20] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 };
+	uint64_t index;
+	uint64_t i;
+	for (i = 0; i < 19; i++)
+		if (mas[i] == n) {
+			index = i;
+			break;
+		}
+		else index = 0;
+	return index;
 }
 
 uint64_t nextPrime(uint64_t value) {
 // вставьте код функции
+  uint64_t mas[20] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 };
+  uint64_t i;
+  uint64_t next;
+  if (nPrime(value) == 0)
+  	return next = 0;
+  else {
+  	for (i = nPrime(value) + 1; i < 19; i++)
+  		if (checkPrime(mas[i])) {
+  			next = mas[i];
+  			break;
+  			return next;
+  		}
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
 // вставьте код функции
+  uint64_t mas[20] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 };
+	uint64_t i;
+	uint64_t sum = 0;
+	if (nPrime(hbound) == 0)
+		sum = 0;
+	else {
+		for (i = 0; i < nPrime(hbound); i++) {
+			sum += mas[i];
+		}
+	}
+	return sum;
 }
