30378c1de946b9202ec57efce4c88891032931c5
viktoria shegurova <137227112+shegurovaviktoria@users.noreply.github.com>
2024-04-21 19:04:24 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,52 +3,52 @@
 #include "alg.h"
 
 bool checkPrime(uint64_t value) {
- uint64_t i;
- if (value < 2) return false;
- else if (value == 2) return true;
- else {
-  for (i = 2; i < value; i++) {
-   if (value % i == 0) return false;
+  uint64_t i;
+  if (value < 2) return false;
+  else if (value == 2) return true;
+  else {
+    for (i = 2; i < value; i++) {
+    if (value % i == 0) return false;
+   }
   }
- }
-return true;
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
- uint64_t i = 1;
- uint64_t temp = 2;
- while (!checkPrime(++temp)) {
-  ++i;
+  uint64_t i = 1;
+  uint64_t temp = 2;
+  while (!checkPrime(++temp)) {
+    ++i;
   }
- return i;
+  return i;
 }
 
 uint64_t nextPrime(uint64_t value) {
- uint64_t mas[20] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
- uint64_t i;
- uint64_t next;
- if (nPrime(value) == 0)
-  return next = 0;
- else {
-  for (i = nPrime(value) + 1; i < 19; i++)
-   if (checkPrime(mas[i])) {
-    next = mas[i];
-    break;
-    return next;
-   }
+  uint64_t mas[20] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+  uint64_t i;
+  uint64_t next;
+  if (nPrime(value) == 0)
+    return next = 0;
+  else {
+    for (i = nPrime(value) + 1; i < 19; i++)
+    if (checkPrime(mas[i])) {
+      next = mas[i];
+      break;
+      return next;
+    }
   }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
- uint64_t mas[20] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
- uint64_t i;
- uint64_t sum = 0;
- if (nPrime(hbound) == 0)
-  sum = 0;
- else {
-  for (i = 0; i < nPrime(hbound); i++) {
-   sum += mas[i];
+  uint64_t mas[20] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+  uint64_t i;
+  uint64_t sum = 0;
+  if (nPrime(hbound) == 0)
+    sum = 0;
+  else {
+    for (i = 0; i < nPrime(hbound); i++) {
+      sum += mas[i];
+    }
   }
- }
- return sum;
+  return sum;
 }
